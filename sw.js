fa9208a6357af27164156298fead9308be69b37f
pattanapongsocial-oss <pattanapong.social@gmail.com>
2026-08-31 19:47:31 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ยอดดอย service worker — เปิดใช้งานได้แม้ออฟไลน์ */
-var CACHE = 'yoddoi-v12';
+var CACHE = 'yoddoi-v13';
 var SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png',
              './icon-maskable-192.png', './icon-maskable-512.png', './apple-touch-icon.png', './favicon.png'];
 self.addEventListener('install', function (e) {
